dbaa68307c94b54bcc3d9b7f453a177ec0d75a1f
Salim Benchekroun <salimben60@gmail.com>
2016-03-12 13:46:37 -0800

    Init program

--- index.ios.js
+++ index.ios.js
@@ -19,7 +19,7 @@ class AwesomeProject extends Component {
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          To get started, edit index.ios.js. Adding Commit Message
         </Text>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
